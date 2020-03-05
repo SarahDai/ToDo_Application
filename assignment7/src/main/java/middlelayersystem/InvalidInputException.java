bae9a3b18401918c2d4f2229752116c3be78ca2e
yafei wang <wang.yaf@husky.neu.edu>
2020-03-05 14:13:40 -0800

    upload exception

---
+++ assignment7/src/main/java/middlelayersystem/InvalidInputException.java
@@ -0,0 +1,15 @@
+
+public class InvalidInputException extends Exception {
+  /**
+   * Constructs a new exception with the specified detail message.  The
+   * cause is not initialized, and may subsequently be initialized by
+   * a call to {@link #initCause}.
+   *
+   * @param msg the detail message. The detail message is saved for
+   *                later retrieval by the {@link #getMessage()} method.
+   */
+  public InvalidInputException(String msg){
+    super(msg);
+  }
+
+}
