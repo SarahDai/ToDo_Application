6afea4eedc629ca9e82b35a3db69223edbc8d500
sarahdai <DAI.XINYU@HUSKY.NEU.EDU>
2020-03-23 22:16:07 -0700

    Add doc for Deliver

--- assignment8/src/main/java/communicationautomation/CommunicationAutoGenerator.java
+++ assignment8/src/main/java/communicationautomation/CommunicationAutoGenerator.java
@@ -2,6 +2,7 @@ package communicationautomation;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * The type CommunicationAutoGenerator, served as coordinator for input output generation.
@@ -51,5 +52,33 @@ public class CommunicationAutoGenerator {
       }
     }
     System.out.println(String.format("Successfully generate files in %s folder.", deliverHandler.getOutputDir()));
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    CommunicationAutoGenerator that = (CommunicationAutoGenerator) o;
+    return Objects.equals(templateParsers, that.templateParsers) &&
+        Objects.equals(csvParser, that.csvParser) &&
+        Objects.equals(deliverHandler, that.deliverHandler);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(templateParsers, csvParser, deliverHandler);
+  }
+
+  @Override
+  public String toString() {
+    return "CommunicationAutoGenerator{" +
+        "templateParsers=" + templateParsers +
+        ", csvParser=" + csvParser +
+        ", deliverHandler=" + deliverHandler +
+        '}';
   }
 }
--- assignment8/src/main/java/communicationautomation/Options.java
+++ assignment8/src/main/java/communicationautomation/Options.java
@@ -3,6 +3,10 @@ package communicationautomation;
 import java.util.*;
 
 
+/**
+ * The type Options, represents a collection of Option objects, which
+ * describe the possible options for a command-line..
+ */
 public class Options {
   private Map<String, Option> allOptions;
   private Map<String, OptionGroup> optionGroups;
@@ -10,32 +14,68 @@ public class Options {
   private String usage;
 
 
+  /**
+   * Instantiates a new Options.
+   */
   public Options() {
     allOptions = new HashMap<>();
     optionGroups = new HashMap<>();
     requiredOptions = new ArrayList<>();
   }
 
+  /**
+   * Gets all options.
+   *
+   * @return the all options
+   */
   public Map<String, Option> getAllOptions() {
     return allOptions;
   }
 
+  /**
+   * Gets option groups.
+   *
+   * @return the option groups
+   */
   public Collection<OptionGroup> getOptionGroups() {
     return new HashSet<OptionGroup>(optionGroups.values());
   }
 
+  /**
+   * Has option group boolean.
+   *
+   * @param option the option
+   * @return the boolean
+   */
   public boolean hasOptionGroup(Option option) {
     return this.optionGroups.containsKey(option.getGroup());
   }
 
+  /**
+   * Gets option group.
+   *
+   * @param option the option
+   * @return the option group
+   */
   public OptionGroup getOptionGroup(Option option) {
     return this.optionGroups.get(option.getGroup());
   }
 
+  /**
+   * Gets required options.
+   *
+   * @return the required options
+   */
   public List<String> getRequiredOptions() {
     return this.requiredOptions;
   }
 
+  /**
+   * Add option group options.
+   *
+   * @param group the group
+   * @return the options
+   */
   public Options addOptionGroup(OptionGroup group) {
     for (Option option: group.getOptions()) {
       if (group.isRequired()) {
@@ -47,6 +87,12 @@ public class Options {
     return this;
   }
 
+  /**
+   * Add option options.
+   *
+   * @param option the option
+   * @return the options
+   */
   public Options addOption(Option option) {
     if (option.isRequired()) {
       requiredOptions.add(option.getName());
@@ -55,18 +101,40 @@ public class Options {
     return this;
   }
 
+  /**
+   * Has option boolean.
+   *
+   * @param opt the opt
+   * @return the boolean
+   */
   public boolean hasOption(String opt) {
     return this.allOptions.containsKey(opt);
   }
 
+  /**
+   * Gets option.
+   *
+   * @param opt the opt
+   * @return the option
+   */
   public Option getOption(String opt) {
     return this.allOptions.get(opt);
   }
 
+  /**
+   * Sets usage.
+   *
+   * @param usage the usage
+   */
   public void setUsage(String usage) {
     this.usage = usage;
   }
 
+  /**
+   * Gets usage.
+   *
+   * @return the usage
+   */
   public String getUsage() {
     return this.usage;
   }
