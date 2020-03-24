f56479b98dbe6c304c4bfc6131c809fb8be230dd
sarahdai <DAI.XINYU@HUSKY.NEU.EDU>
2020-03-24 14:07:35 -0700

    Add docs for Main

--- assignment8/src/main/java/communicationautomation/Main.java
+++ assignment8/src/main/java/communicationautomation/Main.java
@@ -4,8 +4,19 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * The Main entry of the Communication Automation Generator system.
+ * User specifies the type of ICSVParser, ITemplateParser, IDeliverHandler here.
+ * The process would then generator the files into the desired output with information
+ * indicating whether it's successful or anything went wrong.
+ */
 public class Main {
 
+  /**
+   * The entry point of application.
+   *
+   * @param args the input arguments
+   */
   public static void main(String[] args) {
     CommandLineParser commandLineParser = new CommandLineParser(Rules.getOptions());
 
