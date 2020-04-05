d7944085743acfecf1a4f4e36e0c8f0ac0d541ac
sarahdai <DAI.XINYU@HUSKY.NEU.EDU>
2020-04-05 16:56:07 -0700

    Add constant field for different groups

---
+++ assignment9/src/main/java/RequestConstants.java
@@ -0,0 +1,7 @@
+package todotrackingsystem;
+
+public class RequestConstants {
+  public static final String ADD_REQUEST = "--add-todo";
+  public static final String COMPLETE_REQUEST = "--complete-todo";
+  public static final String DISPLAY_REQUEST = "--display";
+}
