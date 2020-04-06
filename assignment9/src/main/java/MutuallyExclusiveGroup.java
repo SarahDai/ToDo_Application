9b84ac8f39efb2ea3cc39f79bc8250102b66ffc9
lingya <hly19971203@hotmail.com>
2020-04-05 20:00:25 -0700

    improve the time complexity of checking in option groups, set the individual option in validArgs

--- assignment9/src/main/java/MutuallyExclusiveGroup.java
+++ assignment9/src/main/java/MutuallyExclusiveGroup.java
@@ -1,45 +1,45 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import utils.InvalidArgumentException;
 import utils.ListFormatter;
 
 public class MutuallyExclusiveGroup extends OptionGroup {
-    private Map<String, List<String>> conflictedOptions;
+    private Set<String> conflictedOptions;
 
     public MutuallyExclusiveGroup(String name, boolean isRequired) {
         super(name, isRequired);
-        this.conflictedOptions = new HashMap<>();
+        this.conflictedOptions = new HashSet<>();
     }
 
     public void addConflictedOption(List<Option> options) throws InvalidArgumentException {
         checkOptions(options);
-        List<String> optionNames = flattenOptions(options);
-        for (String optName : optionNames) {
-            List<String> temp = this.conflictedOptions.getOrDefault(optName, new ArrayList<>());
-            temp.addAll(optionNames);
-            temp.remove(optName);
-            this.conflictedOptions.put(optName, temp);
+        for (Option option : options) {
+            this.conflictedOptions.add(option.getName());
         }
     }
 
     @Override
     public void checkValid(List<Option> options) throws InvalidArgumentException {
         basicCheck(options);
-        List<String> optionNames = flattenOptions(options);
-        for (String optName : optionNames) {
-            if (conflictedOptions.containsKey(optName)) {
-                List<String> conflicted = new ArrayList<>();
-                for (String conflictName : conflictedOptions.get(optName)) {
-                    if (optionNames.contains(conflictName)) conflicted.add(conflictName);
-                }
-                if (!conflicted.isEmpty())
+        String appear = null;
+        for (Option opt : options) {
+            String optName = opt.getName();
+            if (this.conflictedOptions.contains(optName))
+                if (appear == null) {
+                    appear = optName;
+                } else {
                     throw new InvalidArgumentException(
                         String.format("%s provided, it cannot combined with %s.",
-                            optName, ListFormatter.format(conflicted)));
-            }
+                        appear, optName));
+                }
+
         }
+
+
     }
 
 
