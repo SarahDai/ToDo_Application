9052e389d002a38617d11a6b2438474c63d7c6f8
sarahdai <DAI.XINYU@HUSKY.NEU.EDU>
2020-04-05 22:36:04 -0700

    Add constants

---
+++ assignment9/src/main/java/RequestConstants.java
@@ -0,0 +1,6 @@
+
+public class RequestConstants {
+  public static final String ADD_REQUEST = "--add-todo";
+  public static final String COMPLETE_REQUEST = "--complete-todo";
+  public static final String DISPLAY_REQUEST = "--display";
+}
