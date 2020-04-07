7f2c9eddd6bbc5960c44434500331c4f81e0be2e
sarahdai <DAI.XINYU@HUSKY.NEU.EDU>
2020-04-07 02:17:33 -0700

    Add doc

--- assignment9/src/main/java/todotrackingsystem/view/DisplayToDoList.java
+++ assignment9/src/main/java/todotrackingsystem/view/DisplayToDoList.java
@@ -3,7 +3,17 @@ package todotrackingsystem.view;
 import java.util.List;
 import todotrackingsystem.model.ToDoItem;
 
+/**
+ * The type Display to do list.
+ */
 public class DisplayToDoList {
+
+  /**
+   * Display the todolist to the users.
+   *
+   * @param todoList the todo list to display
+   * @return the string to be displayed
+   */
   public static String display(List<ToDoItem> todoList) {
     StringBuilder sb = new StringBuilder();
 
--- assignment9/src/main/java/todotrackingsystem/view/Main.java
+++ assignment9/src/main/java/todotrackingsystem/view/Main.java
@@ -5,7 +5,16 @@ import todotrackingsystem.utils.InvalidArgumentException;
 import todotrackingsystem.utils.Rules;
 import todotrackingsystem.controller.RequestController;
 
+/**
+ * The type Main.
+ */
 public class Main {
+
+  /**
+   * The entry point of Todo tracking system application.
+   *
+   * @param args the input arguments
+   */
   public static void main(String[] args) {
     try {
       CommandLineParser commandLineParser = new CommandLineParser(Rules.getOptions());
